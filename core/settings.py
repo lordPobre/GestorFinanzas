595fedcbdbcecaf5fc8ae5f595035a4720fe38ee
Carlos Lopez <clopez.ti@vcchile.cl>
2026-01-10 22:35:24 -0300

    Corregir STATIC_ROOT para Render

--- core/settings.py
+++ core/settings.py
@@ -128,11 +128,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = 'dashboard'
 # Cuando sales, te manda al login de nuevo
