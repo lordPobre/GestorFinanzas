e46971523415cef27c9afb06517fbd036a0ac565
Carlos Lopez <clopez.ti@vcchile.cl>
2026-08-23 12:15:55 -0400

    Fix base.css

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'finanzas.context_processors.moneda_usuario',   # ← ESTA LÍNEA
+                'finanzas.context_processors.moneda_usuario',   
             ],
         },
     },
